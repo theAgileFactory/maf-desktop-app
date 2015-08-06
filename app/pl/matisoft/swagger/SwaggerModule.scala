1c2cf6c8bebfb726a5dc08b894c26367c81847f2
pycloux <me@pycloux.com>
2015-08-06 14:47:44 +0200

    Adding swagger implementation compatible with Play2.4

---
+++ app/pl/matisoft/swagger/SwaggerModule.scala
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2014 Reverb Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.matisoft.swagger
+
+import play.api.inject.{Binding, Module}
+import play.api.{Configuration, Environment}
+
+class SwaggerModule extends Module {
+
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] =  Seq(
+     bind[SwaggerPlugin].toProvider[SwaggerPluginProvider].eagerly(),
+     bind[ApiHelpController].toSelf.eagerly()
+  )
+
+}
