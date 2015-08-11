5b5a7b78cc03022f9a394a8a417c0f7851073789
pycloux <me@pycloux.com>
2015-08-11 10:56:32 +0200

    Conversion of te ReportingUtils into a full fledge service to support depdency injection

---
+++ app/utils/reporting/IReportingUtils.java
@@ -0,0 +1,37 @@
+package utils.reporting;
+
+import java.sql.Connection;
+import java.util.Map;
+
+import play.mvc.Http.Context;
+import models.reporting.Reporting;
+
+public interface IReportingUtils {
+
+    /**
+     * Generate a jasper a report and place the file in the personal space.
+     * 
+     * @param context
+     *            the current HTTP context
+     * @param report
+     *            the report
+     * @param language
+     *            the language (en, fr, de)
+     * @param format
+     *            the format
+     * @param reportParameters
+     *            the report parameters
+     */
+    public abstract void generate(Context context, Reporting report, String language, Reporting.Format format, Map<String, Object> reportParameters);
+
+    /**
+     * Get the data adapter (DB connection).
+     */
+    public abstract Connection getDataAdapter();
+
+    /**
+     * Load the report definitions.
+     */
+    public abstract void loadDefinitions();
+
+}
