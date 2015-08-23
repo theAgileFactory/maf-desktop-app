df99c5c3420758c564e79653369e5b3e780bbaf2
pycloux <me@pycloux.com>
2015-08-23 20:16:33 +0200

    Adding a commons.scala for services injection in templates

---
+++ app/views/commons.scala
@@ -0,0 +1,15 @@
+package views.html
+
+/**
+ * This class provides a few scala utilities to be reused in various
+ * parts
+ */
+object commons extends framework.handlers.DefaultContextInjector{
+	//-----------------------------------------------------------------------------------
+	// Injection of common services into the scala templates
+	// WARNING: this assumes that these services have been injected into the Http.Context
+	// otherwise the values will be null.
+	// Please see framework.handlers.AbstractRequestHandler
+	//-----------------------------------------------------------------------------------
+	val _dataSyndication=play.mvc.Http.Context.current().args.get(classOf[services.datasyndication.IDataSyndicationService].getName).asInstanceOf[services.datasyndication.IDataSyndicationService]
+}
