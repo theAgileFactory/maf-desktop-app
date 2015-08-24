323d1e84535fc66215fe9f55337112ca28be2237
pycloux <me@pycloux.com>
2015-08-24 21:42:46 +0200

    Creation of a generic ErrorHandler

--- app/views/commons.scala
+++ app/views/commons.scala
@@ -4,7 +4,7 @@ package views.html
  * This class provides a few scala utilities to be reused in various
  * parts
  */
-object commons extends framework.handlers.DefaultContextInjector{
+object commons extends framework.handlers.ViewsInjector{
 	//-----------------------------------------------------------------------------------
 	// Injection of common services into the scala templates
 	// WARNING: this assumes that these services have been injected into the Http.Context
