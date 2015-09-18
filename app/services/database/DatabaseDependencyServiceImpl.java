1149cdf80c364011d449fd7acb2e4959a0bcd5f1
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-09-18 13:40:56 +0200

    Add patcher for release 8 (refs #1553)

--- app/services/database/DatabaseDependencyServiceImpl.java
+++ app/services/database/DatabaseDependencyServiceImpl.java
@@ -1,10 +1,17 @@
 package services.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import constants.IMafConstants;
 import framework.services.database.AbstractDatabaseDependencyServiceImpl;
 import framework.services.database.IDatabaseDependencyService;
+import models.framework_models.account.SystemLevelRoleType;
+import models.framework_models.account.SystemPermission;
+import models.framework_models.common.TextCustomAttributeValue;
 import play.Configuration;
 import play.Environment;
 import play.Logger.ALogger;
@@ -12,12 +19,27 @@ import play.db.ebean.EbeanConfig;
 import play.inject.ApplicationLifecycle;
 
 /**
- * A concrete implementation for the {@link AbstractDatabaseDependencyServiceImpl} and {@link IDatabaseDependencyService}
+ * A concrete implementation for the
+ * {@link AbstractDatabaseDependencyServiceImpl} and
+ * {@link IDatabaseDependencyService}.
+ * 
  * @author Pierre-Yves Cloux
  */
 @Singleton
 public class DatabaseDependencyServiceImpl extends AbstractDatabaseDependencyServiceImpl {
 
+    /**
+     * Constructor the dependency implementation.
+     * 
+     * @param lifecycle
+     *            the lifecycle service
+     * @param environment
+     *            the environment service
+     * @param configuration
+     *            the configuration service
+     * @param ebeanConfig
+     *            the eban configuration
+     */
     @Inject
     public DatabaseDependencyServiceImpl(ApplicationLifecycle lifecycle, Environment environment, Configuration configuration, EbeanConfig ebeanConfig) {
         super(lifecycle, environment, configuration, ebeanConfig);
@@ -30,6 +52,43 @@ public class DatabaseDependencyServiceImpl extends AbstractDatabaseDependencySer
 
     @Override
     public void patch(ALogger log) {
-        log.info("Patching the database");
+
+        /**
+         * Delete planning package filter configuration for all users.
+         */
+
+        List<TextCustomAttributeValue> packageFilters = TextCustomAttributeValue.find.where().eq("deleted", false)
+                .eq("customAttributeDefinition.deleted", false).eq("customAttributeDefinition.uuid", IMafConstants.PACKAGES_FILTER_STORAGE_PREFERENCE)
+                .findList();
+
+        for (TextCustomAttributeValue packageFilter : packageFilters) {
+            log.info("delete TextCustomAttributeValue with id " + packageFilter.id);
+            packageFilter.doDelete();
+        }
+
+        /**
+         * Add PARTNER_SYNDICATION_PERMISSION to roles with permission
+         * ADMIN_USER_ADMINISTRATION_PERMISSION.
+         */
+
+        List<SystemLevelRoleType> roles = SystemLevelRoleType.find.where().eq("deleted", false).eq("systemPermissions.deleted", false)
+                .eq("systemPermissions.name", IMafConstants.ADMIN_USER_ADMINISTRATION_PERMISSION).findList();
+
+        SystemPermission permission = SystemPermission.getSystemPermissionByName(IMafConstants.PARTNER_SYNDICATION_PERMISSION);
+
+        for (SystemLevelRoleType role : roles) {
+            log.info("role with name " + role.getName() + " has the permission " + IMafConstants.ADMIN_USER_ADMINISTRATION_PERMISSION);
+
+            if (!SystemLevelRoleType.hasPermission(role.id, IMafConstants.PARTNER_SYNDICATION_PERMISSION)) {
+                log.info("but hasn't the permission " + IMafConstants.PARTNER_SYNDICATION_PERMISSION + ": add it");
+                if (role.systemPermissions == null) {
+                    role.systemPermissions = new ArrayList<SystemPermission>();
+                }
+                role.systemPermissions.add(permission);
+                role.save();
+            } else {
+                log.info("but has already the permission " + IMafConstants.PARTNER_SYNDICATION_PERMISSION + ": do nothing");
+            }
+        }
     }
 }
