50c3632072985228ae3d45748d92db6564c041c3
pycloux <me@pycloux.com>
2015-09-28 19:07:36 +0200

    Save intermediate development regarding workspaces

---
+++ app/dao/admin/WorkspaceDao.java
@@ -0,0 +1,12 @@
+package dao.admin;
+
+/**
+ * The DAO which manages the access to data related to workspaces
+ * @author Pierre-Yves Cloux
+ */
+public class WorkspaceDao {
+
+    public WorkspaceDao() {
+    }
+
+}
