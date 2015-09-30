ec1161f3c1fc108ab188417f81c38ff6c7f4af23
pycloux <me@pycloux.com>
2015-09-30 16:33:45 +0200

    Specification of the generic structure for Angular enabled modules

--- app/controllers/admin/WorkspaceController.java
+++ app/controllers/admin/WorkspaceController.java
@@ -1,5 +1,12 @@
 package controllers.admin;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import framework.services.api.IApiControllerUtilsService;
+import framework.services.configuration.II18nMessagesPlugin;
 import play.libs.F.Promise;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -9,11 +16,29 @@ import play.mvc.Result;
  * @author Pierre-Yves Cloux
  */
 public class WorkspaceController extends Controller{
-
+    @Inject
+    private IApiControllerUtilsService apiControllerUtilsService;
+    @Inject
+    private II18nMessagesPlugin i18nMessagePlugin;
+    
     public WorkspaceController() {
     }
     
-    public Promise<Result> displayWorkspaceConfiguration(){
-        return  Promise.promise(() -> ok(views.html.admin.workspace.edition.render("Title")));
+    /**
+     * Display the application start screen
+     * @return
+     */
+    public Promise<Result> display(){
+        Map<String, String> messages=new HashMap<>();
+        messages.put("members.button.title", "Members");
+        return  Promise.promise(() -> ok(views.html.admin.workspace.edition.render("Title", getApiControllerUtilsService().convertAsJsonString(messages))));
+    }
+
+    private IApiControllerUtilsService getApiControllerUtilsService() {
+        return apiControllerUtilsService;
+    }
+
+    private II18nMessagesPlugin getI18nMessagePlugin() {
+        return i18nMessagePlugin;
     }
 }
