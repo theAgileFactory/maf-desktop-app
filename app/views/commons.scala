2870a40d9b26a6bb460ca8b2934365137b2586e5
jkohlertaf <johann.kohler@the-agile-factory.com>
2016-01-13 16:24:14 +0100

    Checkstyle correction

--- app/views/commons.scala
+++ app/views/commons.scala
@@ -5,13 +5,13 @@ package views.html
  * parts
  */
 object commons extends framework.handlers.ViewsInjector{
-	//-----------------------------------------------------------------------------------
-	// Injection of common services into the scala templates
-	// WARNING: this assumes that these services have been injected into the Http.Context
-	// otherwise the values will be null.
-	// Please see framework.handlers.AbstractRequestHandler
-	//-----------------------------------------------------------------------------------
-	val _dataSyndication=play.mvc.Http.Context.current().args.get(classOf[services.datasyndication.IDataSyndicationService].getName).asInstanceOf[services.datasyndication.IDataSyndicationService]
+    //-----------------------------------------------------------------------------------
+    // Injection of common services into the scala templates
+    // WARNING: this assumes that these services have been injected into the Http.Context
+    // otherwise the values will be null.
+    // Please see framework.handlers.AbstractRequestHandler
+    //-----------------------------------------------------------------------------------
+    val _dataSyndication=play.mvc.Http.Context.current().args.get(classOf[services.datasyndication.IDataSyndicationService].getName).asInstanceOf[services.datasyndication.IDataSyndicationService]
     val _echannelService=play.mvc.Http.Context.current().args.get(classOf[services.echannel.IEchannelService].getName).asInstanceOf[services.echannel.IEchannelService]
     val _securityService=play.mvc.Http.Context.current().args.get(classOf[framework.security.ISecurityService].getName).asInstanceOf[framework.security.ISecurityService]
     val _notificationService=play.mvc.Http.Context.current().args.get(classOf[framework.services.notification.INotificationManagerPlugin].getName).asInstanceOf[framework.services.notification.INotificationManagerPlugin]
