f074e42194945c6624f4ac70d379de4b29c4c975
Marc Schaer <marc110191@gmail.com>
2016-04-01 15:08:45 +0200

    update deploy script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-mkdir -p git/snapshots/com/agifac/lib/app-framework/$1
-mkdir -p git/snapshots/com/agifac/maf/maf-desktop-datamodel/$2
+mkdir -p git/snapshots/com/agifac/maf/maf-desktop-app/$2
 
-cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.jar git/snapshots/com/agifac/lib/app-framework/$1
-cp ~/.m2/repository/com/agifac/lib/app-framework/$1/*.pom git/snapshots/com/agifac/lib/app-framework/$1
-cp ~/.m2/repository/com/agifac/maf/maf-desktop-datamodel/$1/*.jar git/snapshots/com/agifac/maf/maf-desktop-datamodel/$1
-cp ~/.m2/repository/com/agifac/maf/maf-desktop-datamodel/$1/*.pom git/snapshots/com/agifac/maf/maf-desktop-datamodel/$1
+cp ~/.m2/repository/com/agifac/maf/desktop/maf-desktop-app/$1/*.jar git/snapshots/com/agifac/maf/desktop/maf-desktop-app/$1
+cp ~/.m2/repository/com/agifac/maf/desktop/maf-desktop-app/$1/*.pom git/snapshots/com/agifac/maf/desktop/maf-desktop-app/$1
 
 cd git
 git init
