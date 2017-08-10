363052464adb2a4aab06de26a3950133f06e963d
Guillaume Petit <guillaume.petit30@gmail.com>
2017-08-10 09:55:00 +0200

    Add scripts to manage database development

---
+++ development/tools/framework_db_new.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/env.cfg
+
+cd $BIZDOCK_GIT_ROOT$DBMDL_FRAMEWORK_PROJECT/src/main/resources/scripts/
+./migrate.sh new $1 --path=../repo --env=development
